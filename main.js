38fdfff50a7e9e5e899e7064a8b9110177499ffd
syk-kakinotane <sayakaaaaaaa.828@gmail.com>
2022-07-04 18:27:45 +0900

    updated

--- main.js
+++ main.js
@@ -15,6 +15,12 @@ mask.addEventListener("click", function (){
     mask.classList.toggle("active");
 })
 
+navi.addEventListener("click", function (){
+  navi.classList.toggle("active"); 
+  mask.classList.toggle("active"); 
+  hamburger.classList.toggle("closeBtn");
+})
+
 $('.pickupSlick').slick({
     arrows: false,
     centerMode: true,
@@ -30,3 +36,20 @@ $('.pickupSlick').slick({
       }
     ]
   });
+
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+  for (let i = 0; i < smoothScrollTrigger.length; i++){
+    smoothScrollTrigger[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      let href = smoothScrollTrigger[i].getAttribute('href');
+      let targetElement = document.getElementById(href.replace('#', ''));
+      const rect = targetElement.getBoundingClientRect().top;
+      const offset = window.pageYOffset;
+      const gap = 120;
+      const target = rect + offset - gap;
+      window.scrollTo({
+        top: target,
+        behavior: 'smooth',
+      });
+    });
+}
